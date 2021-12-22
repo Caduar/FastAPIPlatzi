adc9da384b21dfe8eba6e611bcc6f294f6d8b5ea
Camilo Duarte <andresduarte_@hotmail.com>
2021-12-21 23:10:50 -0500

    validation_model

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 #Python - Respetar el orden es super importante, que va primero y que va despues.
 from typing import Optional
+from enum import Enum
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body, Query, Path
 app = FastAPI()
 
 #Models
+
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    yellow = "blonde"
+    red = "red"
+
 class Location(BaseModel):
     city: str
     state: str
     country: str
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=200
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 @app.get("/")
 def home():
