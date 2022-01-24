ef33944610cfb574572eac58e25a20988548082c
Camilo Duarte <andresduarte_1256@hotmail.com>
2022-01-24 17:32:43 -0500

    iniciamos curso parte 2

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+    password: str = Field(..., min_length = 8)
     #class Config:
     #    schema_extra = {
     #        "example": {
@@ -72,12 +73,14 @@ def show_person(
             None, min_length=1,
             max_length=50,
             title= "Person Name",
-            description= "This is the person name. It's between 1 and 50 characters"
+            description= "This is the person name. It's between 1 and 50 characters",
+            example= "Rocio"
         ),
         age: str = Query(
             ...,
             title="Person Age",
-            descriptrion="This is the person Age, It is required"
+            descriptrion="This is the person Age, It is required",
+            example= 25
         )
 ):
     return{name: age}
@@ -86,7 +89,7 @@ def show_person(
 
 @app.get("/person/detail/{person_id}")
 def show_person(
-        person_id: int = Path(..., gt=0)
+        person_id: int = Path(..., gt=0, example=123)
 ):
     return  {person_id: "It exist!"}
 
@@ -97,7 +100,8 @@ def update_person(
             ...,
             title="Person ID",
             description= "This is the person ID",
-            gt=0
+            gt=0,
+            example=25
         ),
         person: Person = Body(...),
         location: Location = Body(...)
